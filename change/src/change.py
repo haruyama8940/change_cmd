b90accf695094c95f54306185dff840d42061526
MasayaOkada <masa.okd888@gmail.com>
2021-03-23 15:51:56 +0900

    otuotu

--- change/src/change.py
+++ change/src/change.py
@@ -6,23 +6,28 @@ from std_msgs.msg import Int8
 
 def cb (self,date):
     self.vel = date
-def ch_cb(messege)
+def ch_cb(messege):
     global ch
     ch = messege.data
+    
+	if ch == 1:
+        //終了nav_velへ書き換え
+    		nav_vel_pub.publish(self.vel)
+	if ch == 2:
+        //cmd_vel優位
+    		cmd_vel_pub.publish(self.vel)
 
 def __init__(self):
-    rospy.init_node('cmd_change')
+    rospy.init_node('change')
 
-	self.vel_sub = rospy.Subscriber("/icart_mini/cmd_vel", Twist, self.cb)
+    self.vel_sub = rospy.Subscriber("/icart_mini/cmd_vel", Twist, self.cb)
     self.ch_sub =  nh.subscribe("ch_cmd", Int8, ch_cb)
     self.nav_vel_pub = rospy.Publisher('nav_vel',Twist,queue_size=10)
     self.cmd_vel_pub = rospy.Publisher("cmd_vel",Twist,queue_size=10)
 
 
-        if ch == 1:
-        //終了nav_velへ書き換え
-            nav_vel_pub.publish(self.vel)
 
-        if ch == 2:
-        //cmd_vel優位
-            cmd_vel_pub.publish(self.vel)
+
+if __name__ == '__main__':
+	ch = change_move()
+	
